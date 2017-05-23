f50cce965140a864281cdb6c0451c8f87df8dd37
qinss <qinshishuai@qinshishuaideMacBook-Pro.local>
2017-05-23 08:30:08 +0800

    Initial Commit

---
+++ eHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eHome
+//
+//  Created by 秦世帅 on 2017/5/23.
+//  Copyright © 2017年 whsmwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eHome/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  eHome
+//
+//  Created by 秦世帅 on 2017/5/23.
+//  Copyright © 2017年 whsmwy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
